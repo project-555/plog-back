e4491b1fd5da7cc8594027da0f3b72c006c32c2a
YangTaeyoung <0130yang@gmail.com>
2022-09-06 23:54:16 +0900

    feat: 참고용 Blog Service 스켈레톤 작성

---
+++ src/main/java/com/plogcareers/backend/blog/domain/entity/Posting.java
@@ -0,0 +1,10 @@
+package com.plogcareers.backend.blog.domain.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(schema = "plog_blog", name = "posting")
+public class Posting {
+    // TODO: 작성 예정
+}
--- src/main/java/com/plogcareers/backend/blog/repository/BlogRepository.java
+++ src/main/java/com/plogcareers/backend/blog/repository/BlogRepository.java
@@ -1,6 +1,6 @@
 package com.plogcareers.backend.blog.repository;
 
-import com.plogcareers.backend.blog.entity.Blog;
+import com.plogcareers.backend.blog.domain.entity.Blog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BlogRepository extends JpaRepository<Blog, Long> {
