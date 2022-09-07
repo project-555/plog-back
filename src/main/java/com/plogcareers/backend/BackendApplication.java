f394f4b2b8776de9e8bd0d56563c0d1cdfef4464
YangTaeyoung <0130yang@gmail.com>
2022-09-08 01:38:40 +0900

    chore: Spring 버전 2.7.4로 다운그레이드 feat: swagger 적용 -> http://localhost:8080/swagger-ui/

--- src/main/java/com/plogcareers/backend/BackendApplication.java
+++ src/main/java/com/plogcareers/backend/BackendApplication.java
@@ -3,11 +3,9 @@ package com.plogcareers.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan({"com.plogcareers.backend"})
-@EnableJpaRepositories
 public class BackendApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/plogcareers/backend/blog/domain/entity/Blog.java
+++ src/main/java/com/plogcareers/backend/blog/domain/entity/Blog.java
@@ -5,6 +5,7 @@ import javax.persistence.*;
 
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name="blog", schema = "plog_blog")
