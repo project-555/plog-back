2f0c4dfeb58b78bf57902cb400de53a531f1da8b
Taeyoung Yang <59782504+YangTaeyoung@users.noreply.github.com>
2022-09-27 18:19:17 +0900

    fix/#16: 표준 응답에 따른 Swagger 명세 변경 (#18)
    - SXXXResponse 사용 시 인터페이스로 인해 Swagger 반영 되지 않던 부분 정상적으로 반영되도록 변경
    - 사용하지 않는 Test 코드 제거
    - Swagger v3 -> v2로 변경

--- src/main/java/com/plogcareers/backend/ums/controller/UserController.java
+++ src/main/java/com/plogcareers/backend/ums/controller/UserController.java
@@ -1,18 +1,23 @@
 package com.plogcareers.backend.ums.controller;
 
 import com.plogcareers.backend.common.domain.dto.SDataResponse;
+import com.plogcareers.backend.common.domain.dto.SErrorResponse;
 import com.plogcareers.backend.common.domain.dto.SResponse;
 import com.plogcareers.backend.common.error.ErrorMapper;
 import com.plogcareers.backend.ums.domain.dto.UserJoinRequest;
 import com.plogcareers.backend.ums.domain.dto.UserLoginRequest;
+import com.plogcareers.backend.ums.domain.dto.UserLoginResponse;
 import com.plogcareers.backend.ums.exception.EmailDuplicatedException;
 import com.plogcareers.backend.ums.exception.LoginFailException;
 import com.plogcareers.backend.ums.service.UserService;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.postgresql.util.PSQLException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.lang.reflect.ParameterizedType;
 
 @RequiredArgsConstructor
 @RestController
@@ -24,6 +29,11 @@ public class UserController {
 
     // 회원가입
     @PostMapping("/join")
+    @ApiOperation(value = "회원가입")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "회원가입 정상처리"),
+        @ApiResponse(code = 400, message = "회원가입 실패", response = SErrorResponse.class)
+    })
     public ResponseEntity<SResponse> join(@RequestBody UserJoinRequest request) {
         try {
             userService.join(request);
@@ -35,6 +45,11 @@ public class UserController {
 
     // 이메일 확인
     @GetMapping("/email/chk")
+    @ApiOperation(value = "이메일 중복확인")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "이메일 사용 가능", response = SResponse.class),
+            @ApiResponse(code = 400, message = "이메일 중복", response = SErrorResponse.class)
+    })
     public ResponseEntity<SResponse> emailCheck(@RequestParam String email) {
         try {
             userService.emailCheck(email);
@@ -46,6 +61,12 @@ public class UserController {
 
     // 로그인
     @PostMapping("/login")
+    @ApiOperation(value = "로그인")
+    @ApiResponses(value = {
+            @ApiResponse(code=200, message = "로그인 성공(data)", response = UserLoginResponse.class),
+            @ApiResponse(code = 299, message = "로그인 성공(outer)", response = SDataResponse.class),
+            @ApiResponse(code=400, message = "로그인 실패", response = SErrorResponse.class)
+    })
     public ResponseEntity<SResponse> login(@RequestBody UserLoginRequest request) {
         try {
             var user = userService.login(request);
