6642e7666d1709d20d9470f783724aa4101b78a0
aiaaua <yse00800@gmail.com>
2022-10-29 17:54:09 +0900

    [#25] Swagger annotation 추가

--- src/main/java/com/plogcareers/backend/blog/controller/BlogController.java
+++ src/main/java/com/plogcareers/backend/blog/controller/BlogController.java
@@ -26,15 +26,37 @@ public class BlogController {
 
     private final PostingService postingService;
     private final ErrorMapper errorMapper;
-    // 포스팅 단건을 생성하는 메서드
-    @PostMapping("/posting")
-    public ResponseEntity<SResponse> createPosting(@RequestBody CreatePostingRequest postingRequest) throws TagNotFoundException {
-        Long postingId = postingService.createPosting(postingRequest);
-        return ResponseEntity.status(HttpStatus.CREATED).body(new SDataResponse<>(postingId));
+
+    @ApiOperation(value = "Posting 생성")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "정삭 동작 시"),
+            @ApiResponse(code = 404, message = "태그 없음"),
+            @ApiResponse(code = 500, message = "서버 에러")
     }
-    // 포스팅 단건을 조회하는 메서드
+    )
+    @ResponseStatus(value = HttpStatus.CREATED)
+    @PostMapping("/posting")
+    public ResponseEntity<SResponse> createPosting(@RequestBody CreatePostingRequest postingRequest) {
+        try {
+            Long postingId = postingService.createPosting(postingRequest);
+            return ResponseEntity.status(HttpStatus.CREATED).body(new SDataResponse<>(postingId));
+        } catch (TagNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMapper.toErrorResponse(ErrorCode.TAG_NOT_FOUND));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMapper.toErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR));
+        }
+    }
+
+    @ApiOperation(value = "Posting 단건 조회")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "정상 조회(data)", response = GetPostingResponse.class),
+            @ApiResponse(code = 299, message = "정상 조회(outer)", response = SDataResponse.class),
+            @ApiResponse(code = 404, message = "포스팅 혹은 유저 없음"),
+            @ApiResponse(code = 500, message = "서버 에러")
+    }
+    )
     @GetMapping("/posting/{id}")
-    public ResponseEntity<SResponse> getPosting(@PathVariable Long id)  {
+    public ResponseEntity<SResponse> getPosting(@PathVariable Long id) {
         try {
             GetPostingResponse posting = postingService.getPosting(id);
             return ResponseEntity.status(HttpStatus.OK).body(new SDataResponse<>(posting));
@@ -42,11 +64,19 @@ public class BlogController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMapper.toErrorResponse(ErrorCode.POSTING_NOT_FOUND));
         } catch (UserNotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMapper.toErrorResponse(ErrorCode.USER_NOT_FOUND));
-        } catch (Exception e){
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMapper.toErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR));
         }
     }
-    // 포스팅 단건을 삭제하는 메서드
+
+    @ApiOperation(value = "Posting 삭제")
+    @ApiResponses(value = {
+            @ApiResponse(code = 204, message = "정삭 삭제"),
+            @ApiResponse(code = 404, message = "포스팅 없음"),
+            @ApiResponse(code = 500, message = "서버 에러")
+    }
+    )
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
     @DeleteMapping("/posting/{id}")
     public ResponseEntity<SResponse> deletePosting(@PathVariable Long id) {
         try {
@@ -54,33 +84,41 @@ public class BlogController {
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         } catch (PostingNotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMapper.toErrorResponse(ErrorCode.POSTING_NOT_FOUND));
-        } catch (Exception e){
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMapper.toErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR));
         }
     }
-
 
     @ApiOperation(value = "State 전체조회")
     @ApiResponses(
-            @ApiResponse(code = 200,message = "정삭 동작 시")
+            @ApiResponse(code = 200, message = "정삭 동작 시")
     )
     @GetMapping("/posting/states")
-    public ResponseEntity<SResponse> listStates(){
+    public ResponseEntity<SResponse> listStates() {
         try {
             return ResponseEntity.status(HttpStatus.OK).body(new SDataResponse<>(postingService.listStates()));
-        } catch (Exception e){
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMapper.toErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR));
         }
     }
 
-    // 포스팅 태그를 조회하는 메서드
+    @ApiOperation(value = "Posting Tag 리스트 조회")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "정상 조회(data)", response = ListPostingTagResponse.class),
+            @ApiResponse(code = 299, message = "정상 조회(outer)", response = SDataResponse.class),
+            @ApiResponse(code = 404, message = "포스팅 없음"),
+            @ApiResponse(code = 500, message = "서버 에러")
+    }
+    )
     @GetMapping("/posting/{postingId}/tag")
-    public ResponseEntity GetPostingTag(@PathVariable Long postingId) {
+    public ResponseEntity<SResponse> GetPostingTag(@PathVariable Long postingId) {
         try {
             ListPostingTagResponse listPostingTag = postingService.listPostingTag(postingId);
             return ResponseEntity.status(HttpStatus.OK).body(new SDataResponse<>(listPostingTag));
         } catch (PostingNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMapper.toErrorResponse("POSTING_NOT_FOUND"));
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMapper.toErrorResponse(ErrorCode.POSTING_NOT_FOUND));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMapper.toErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR));
         }
     }
 }
