72e4b05cf3e9f301d82602d098e051d12f74939d
aiaaua <55227276+aiaaua@users.noreply.github.com>
2022-11-02 20:02:03 +0900

    [#26] Category Repo, DTO 개발 및 Error 추가 (#46)
    * [#26] Category Repo, DTO 개발 및 Error 추가
    
    * [#26] BlogId로 Category 조회 시 Sort 기준 정렬 추가

---
+++ src/main/java/com/plogcareers/backend/blog/domain/dto/ListCategoryResponse.java
@@ -0,0 +1,17 @@
+package com.plogcareers.backend.blog.domain.dto;
+
+import com.plogcareers.backend.blog.domain.model.CategoryDTO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ListCategoryResponse {
+    List<CategoryDTO> category;
+}
