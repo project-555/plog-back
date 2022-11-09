21e7f220729c56d8e5838de162fa834441b83bcb
aiaaua <yse00800@gmail.com>
2022-11-09 18:40:23 +0900

    [#23] Tag Repository, Entity, DTO 개발

--- src/main/java/com/plogcareers/backend/blog/repository/TagRepository.java
+++ src/main/java/com/plogcareers/backend/blog/repository/TagRepository.java
@@ -3,5 +3,8 @@ package com.plogcareers.backend.blog.repository;
 import com.plogcareers.backend.blog.domain.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TagRepository extends JpaRepository<Tag, Long> {
+    List<Tag> findTagsByBlogId(Long blogId);
 }
