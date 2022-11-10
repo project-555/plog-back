ec869249c49a1a4c20fd7a6429dc7f93465543f1
aiaaua <yse00800@gmail.com>
2022-11-10 11:19:22 +0900

    [#23] Tag DTO 누락 annotation 추가

--- src/main/java/com/plogcareers/backend/blog/domain/model/TagDTO.java
+++ src/main/java/com/plogcareers/backend/blog/domain/model/TagDTO.java
@@ -1,8 +1,14 @@
 package com.plogcareers.backend.blog.domain.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Builder
+@AllArgsConstructor
 public class TagDTO {
     private Long tagId;
     private String tagName;
