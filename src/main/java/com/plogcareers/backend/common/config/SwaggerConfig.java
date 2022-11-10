43f686036fece34d65f2b0e0cc45351000fd4d16
YangTaeyoung <0130yang@gmail.com>
2022-11-10 23:12:00 +0900

    [#49] JWT 파싱, Swagger Auth 추가, Security 활성화

--- src/main/java/com/plogcareers/backend/common/config/SwaggerConfig.java
+++ src/main/java/com/plogcareers/backend/common/config/SwaggerConfig.java
@@ -1,18 +1,20 @@
 package com.plogcareers.backend.common.config;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Configuration
 @EnableSwagger2
@@ -23,9 +25,12 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .consumes(getConsumeContentTypes())
                 .produces(getProduceContentTypes())
+                .securityContexts(Collections.singletonList(securityContext()))
+                .securitySchemes(List.of(apiKey()))
                 .apiInfo(getApiInfo())
                 .useDefaultResponseMessages(false)
                 .select()
+                .apis(RequestHandlerSelectors.basePackage("com.plogcareers.backend"))
                 .paths(PathSelectors.ant("/**"))
                 .build();
     }
@@ -45,10 +50,28 @@ public class SwaggerConfig {
 
     private ApiInfo getApiInfo() {
         return new ApiInfoBuilder()
-                .title("API")
-                .description("Plog Backend API")
+                .title("Plog Careers API")
+                .description("😆 Welcome Plog Careers Backend API")
                 .contact(new Contact("Plog Backend Swagger", "www.plogcareers.com", "0130yang@gmail.com"))
                 .version("0.0.1")
                 .build();
+    }
+
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return List.of(new SecurityReference("JWT", authorizationScopes));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "X-AUTH-TOKEN", "header");
     }
 }
