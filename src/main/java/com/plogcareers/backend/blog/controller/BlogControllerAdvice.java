12f4e794a66c1dab503a91d1973d9d19326e7187
YangTaeyoung <0130yang@gmail.com>
2022-11-13 20:16:21 +0900

    [#50] Validation 오류 수정, paging validation 추가

--- src/main/java/com/plogcareers/backend/blog/controller/BlogControllerAdvice.java
+++
@@ -1,9 +0,0 @@
-package com.plogcareers.backend.blog.controller;
-
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-
-@RestControllerAdvice
-public class BlogControllerAdvice {
-
-}
--- src/main/java/com/plogcareers/backend/common/advice/CommonControllerAdvice.java
+++ src/main/java/com/plogcareers/backend/common/advice/CommonControllerAdvice.java
@@ -1,7 +1,7 @@
 package com.plogcareers.backend.common.advice;
 
-import com.plogcareers.backend.blog.controller.BlogControllerAdvice;
 import com.plogcareers.backend.common.domain.dto.ErrorResponse;
+import com.plogcareers.backend.common.error.ErrorCode;
 import com.plogcareers.backend.common.exception.InvalidParamException;
 import com.plogcareers.backend.common.exception.UserException;
 import org.slf4j.Logger;
@@ -12,15 +12,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class CommonControllerAdvice {
-    private final Logger logger = LoggerFactory.getLogger(BlogControllerAdvice.class);
+    private final Logger logger = LoggerFactory.getLogger(CommonControllerAdvice.class);
 
     @ExceptionHandler(InvalidParamException.class)
-    protected ResponseEntity<ErrorResponse> handleInvalidParamException(InvalidParamException e) {
-        return ErrorResponse.toUserErrorResponseEntity(e.getErrorCode(), e.getErrors());
+    public ResponseEntity<ErrorResponse> handleInvalidParamException(InvalidParamException e) {
+        return ErrorResponse.toUserErrorResponseEntity(ErrorCode.ERR_INVALID_PARAMETER, e.getErrors());
     }
 
     @ExceptionHandler(UserException.class)
-    protected ResponseEntity<ErrorResponse> handleUserException(UserException e) {
+    public ResponseEntity<ErrorResponse> handleUserException(UserException e) {
         return ErrorResponse.toUserErrorResponseEntity(e.getErrorCode());
     }
 }
