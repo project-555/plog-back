b4fa5f101eefab15a340ca5d067ce857ecd41388
YangTaeyoung <0130yang@gmail.com>
2022-12-12 01:01:26 +0900

    [#66] 태그 CRUD API 구현

---
+++ src/main/java/com/plogcareers/backend/blog/domain/dto/UpdateTagRequest.java
@@ -0,0 +1,25 @@
+package com.plogcareers.backend.blog.domain.dto;
+
+import com.plogcareers.backend.blog.domain.entity.Tag;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotNull;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class UpdateTagRequest {
+    @NotNull
+    @Length(min = 1, max = 30)
+    String tagName;
+
+    public Tag toTagEntity(Tag tag) {
+        tag.setTagName(this.tagName);
+        return tag;
+    }
+}
