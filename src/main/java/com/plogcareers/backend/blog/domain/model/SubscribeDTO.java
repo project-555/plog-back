97ff190b37bf3a413891fdb541f82269ec5984bd
aiaaua <yse00800@gmail.com>
2023-02-19 23:08:49 +0900

    [#79] 컨벤션에 맞도록 변수명 수정

--- src/main/java/com/plogcareers/backend/blog/domain/model/SubscribeDTO.java
+++ src/main/java/com/plogcareers/backend/blog/domain/model/SubscribeDTO.java
@@ -14,17 +14,17 @@ public class SubscribeDTO {
     public Long id;
 
     @ApiModelProperty(value = "구독한 블로그 ID")
-    public Long blog_id;
+    public Long blogId;
 
     @ApiModelProperty(value = "구독한 블로그 유저 ID")
-    public Long blog_user_id;
+    public Long blogUserId;
 
     @ApiModelProperty(value = "구독한 블로그 이름")
-    public String blog_name;
+    public String blogName;
 
     @ApiModelProperty(value = "구독한 블로그 유저 닉네임")
     public String nickname;
 
     @ApiModelProperty(value = "구독한 블로그 유저 프로필 이미지 url")
-    public String profile_image_url;
+    public String profileImageUrl;
 }
