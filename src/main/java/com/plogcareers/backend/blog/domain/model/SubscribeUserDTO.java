8712011e72deb69e715c66537279d95e95942fb9
aiaaua <yse00800@gmail.com>
2023-02-22 21:52:42 +0900

    [#79] subscribe entity에 blog 객체로 변경, 구독 리스트 서비스로직 구현 변경, SubscribeUserDTO 추가

---
+++ src/main/java/com/plogcareers/backend/blog/domain/model/SubscribeUserDTO.java
@@ -0,0 +1,21 @@
+package com.plogcareers.backend.blog.domain.model;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.*;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class SubscribeUserDTO {
+    @ApiModelProperty(value = "구독한 블로그 유저 ID")
+    public Long blogUserId;
+
+    @ApiModelProperty(value = "구독한 블로그 유저 닉네임")
+    public String nickname;
+
+    @ApiModelProperty(value = "구독한 블로그 유저 프로필 이미지 url")
+    public String profileImageUrl;
+}
