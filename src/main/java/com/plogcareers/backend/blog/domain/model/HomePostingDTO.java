44de3cafb4fb9e78e7e0fd61e6edb17d0f350137
aiaaua <yse00800@gmail.com>
2023-03-12 22:52:29 +0900

    [DP] 홈 포스팅에 blogID 추가

--- src/main/java/com/plogcareers/backend/blog/domain/model/HomePostingDTO.java
+++ src/main/java/com/plogcareers/backend/blog/domain/model/HomePostingDTO.java
@@ -4,14 +4,13 @@ import com.plogcareers.backend.blog.domain.dto.HomePostingUserDTO;
 import lombok.*;
 
 import java.time.LocalDateTime;
-import java.util.Objects;
-
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Getter
 @EqualsAndHashCode
 public class HomePostingDTO {
+    private Long blogID;
     private Long postingID;
     private HomePostingUserDTO homePostingUser;
     private String title;
