9aad8ca6de3a315a794bbaafadde38c1e90c0d25
aiaaua <yse00800@gmail.com>
2023-05-21 22:53:55 +0900

    [#101] 회원 기본 정보 조회, 회원 탈퇴 api 구현

---
+++ src/main/java/com/plogcareers/backend/ums/domain/dto/GetUserInfoResponse.java
@@ -0,0 +1,29 @@
+package com.plogcareers.backend.ums.domain.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class GetUserInfoResponse {
+    @ApiModelProperty(value = "이메일")
+    private String email;
+
+    @ApiModelProperty(value = "닉네임")
+    private String nickname;
+
+    @ApiModelProperty(value = "프로필 이미지 URL")
+    private String profileImageUrl;
+
+    @ApiModelProperty(value = "블로그 제목")
+    private String blogName;
+
+    @ApiModelProperty(value = "블로그 소개글")
+    private String shortIntro;
+
+    @ApiModelProperty(value = "블로그 소개 HTML")
+    private String introHtml;
+}
