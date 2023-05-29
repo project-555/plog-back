26ec1295a9c293b5b72c167861a0c5122bbcac2b
aiaaua <yse00800@gmail.com>
2023-05-29 23:29:32 +0900

    [#101] Auth Controller, Service, unittest 분리

--- src/test/java/com/plogcareers/backend/ums/service/UserServiceTest.java
+++ src/test/java/com/plogcareers/backend/ums/service/UserServiceTest.java
@@ -3,11 +3,7 @@ package com.plogcareers.backend.ums.service;
 import com.plogcareers.backend.blog.domain.entity.Blog;
 import com.plogcareers.backend.blog.repository.BlogRepository;
 import com.plogcareers.backend.ums.domain.dto.GetUserInfoResponse;
-import com.plogcareers.backend.ums.domain.dto.UpdateUserPasswordRequest;
-import com.plogcareers.backend.ums.domain.dto.UpdateUserProfileRequest;
 import com.plogcareers.backend.ums.domain.entity.User;
-import com.plogcareers.backend.ums.exception.IncorrectPasswordException;
-import com.plogcareers.backend.ums.exception.NotProperAuthorityException;
 import com.plogcareers.backend.ums.exception.UserNotFoundException;
 import com.plogcareers.backend.ums.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
@@ -17,7 +13,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 import java.util.Optional;
@@ -32,109 +27,8 @@ public class UserServiceTest {
     @Mock
     BlogRepository blogRepository;
 
-    @Mock
-    PasswordEncoder passwordEncoder;
-
     @InjectMocks
     UserService userService;
-
-    @Test
-    @DisplayName("updateUserProfile - 유저가 없는 경우 테스트")
-    void testUpdateUserProfile_1(){
-        // given
-        when(
-                userRepository.findById(-1L)
-        ).thenReturn(Optional.empty());
-        // when + then
-        Assertions.assertThrows(UserNotFoundException.class, () -> userService.updateUserProfile(1L, UpdateUserProfileRequest.builder().userID(-1L).build()));
-    }
-
-    @Test
-    @DisplayName("updateUserProfile - 로그인한 유저와 요청 유저가 다른 경우 테스트")
-    void testUpdateUserProfile_2(){
-        // given
-        when(
-                userRepository.findById(1L)
-        ).thenReturn(
-                Optional.of(User.builder().id(1L).build())
-        );
-        // when + then
-        Assertions.assertThrows(NotProperAuthorityException.class, () -> userService.updateUserProfile(-1L, UpdateUserProfileRequest.builder().userID(1L).build()));
-    }
-
-    @Test
-    @DisplayName("updateUserProfile - 정상동작")
-    void testUpdateUserProfile_3(){
-        // given
-        when(
-                userRepository.findById(1L)
-        ).thenReturn(
-                Optional.of(User.builder().id(1L).build())
-        );
-        // when
-        userService.updateUserProfile(1L, UpdateUserProfileRequest.builder().userID(1L).build());
-        // then
-        verify(userRepository, times(1)).save(any());
-    }
-
-    @Test
-    @DisplayName("updateUserPassword - 유저가 없는 경우 테스트")
-    void testUpdateUserPassword_1(){
-        // given
-        when(
-                userRepository.findById(-1L)
-        ).thenReturn(Optional.empty());
-        // when + then
-        Assertions.assertThrows(UserNotFoundException.class, () -> userService.updateUserPassword(1L, UpdateUserPasswordRequest.builder().userID(-1L).build()));
-    }
-
-    @Test
-    @DisplayName("updateUserPassword - 로그인한 유저와 요청 유저가 다른 경우 테스트")
-    void testUpdateUserPassword_2(){
-        // given
-        when(
-                userRepository.findById(1L)
-        ).thenReturn(
-                Optional.of(User.builder().id(1L).build())
-        );
-        // when + then
-        Assertions.assertThrows(NotProperAuthorityException.class, () -> userService.updateUserPassword(-1L, UpdateUserPasswordRequest.builder().userID(1L).build()));
-    }
-
-    @Test
-    @DisplayName("updateUserPassword - 현재 패스워드가 다른 경우 테스트")
-    void testUpdateUserPassword_3(){
-        // given
-        when(
-                passwordEncoder.matches("2", "1")
-        ).thenReturn(false);
-        when(
-                userRepository.findById(1L)
-        ).thenReturn(
-                Optional.of(User.builder().id(1L).password("1").build())
-        );
-        // when + then
-        Assertions.assertThrows(IncorrectPasswordException.class, () -> userService.updateUserPassword(1L, UpdateUserPasswordRequest.builder().userID(1L).password("2").build()));
-    }
-
-    @Test
-    @DisplayName("updateUserPassword - 정상동작")
-    void testUpdateUserPassword_4(){
-        // given
-        when(
-                passwordEncoder.matches("1", "1")
-        ).thenReturn(true);
-        when(
-                userRepository.findById(1L)
-        ).thenReturn(
-                Optional.of(User.builder().id(1L).password("1").build())
-        );
-        // when
-        userService.updateUserPassword(1L, UpdateUserPasswordRequest.builder().userID(1L).password("1").build());
-        // then
-        verify(userRepository, times(1)).save(any());
-    }
-
 
     @Test
     @DisplayName("getUserInfo - 유저가 없는 경우 테스트")
@@ -146,25 +40,12 @@ public class UserServiceTest {
                 Optional.empty()
         );
         // when + then
-        Assertions.assertThrows(UserNotFoundException.class, () -> userService.getUserInfo(1L,-1L));
-    }
-
-    @Test
-    @DisplayName("getUserInfo - 로그인한 유저와 요청 유저가 다른 경우 테스트")
-    void testGetUserInfo_2(){
-        // given
-        when(
-                userRepository.findById(1L)
-        ).thenReturn(
-                Optional.of(User.builder().id(1L).build())
-        );
-        // when + then
-        Assertions.assertThrows(NotProperAuthorityException.class, () -> userService.getUserInfo(2L, 1L));
+        Assertions.assertThrows(UserNotFoundException.class, () -> userService.getUser(-1L));
     }
 
     @Test
     @DisplayName("getUserInfo - 정상동작")
-    void testGetUserInfo_3(){
+    void testGetUserInfo_2(){
         User testUser = User.builder()
                 .id(1L)
                 .email("email")
@@ -187,7 +68,7 @@ public class UserServiceTest {
                         .build())
         );
         // when
-        GetUserInfoResponse got = userService.getUserInfo(1L, 1L);
+        GetUserInfoResponse got = userService.getUser(1L);
 
         GetUserInfoResponse want = GetUserInfoResponse.builder()
                                                     .email("email")
@@ -201,42 +82,4 @@ public class UserServiceTest {
         Assertions.assertEquals(got, want);
     }
 
-    @Test
-    @DisplayName("deleteUser - 유저가 없는 경우 테스트")
-    void testDeleteUser_1(){
-        // given
-        when(
-                userRepository.findById(-1L)
-        ).thenReturn(Optional.empty());
-        // when + then
-        Assertions.assertThrows(UserNotFoundException.class, () -> userService.deleteUser(-1L, 1L));
-    }
-
-    @Test
-    @DisplayName("deleteUser - 로그인한 유저와 요청 유저가 다른 경우 테스트")
-    void testDeleteUser_2(){
-        // given
-        when(
-                userRepository.findById(1L)
-        ).thenReturn(
-                Optional.of(User.builder().id(1L).build())
-        );
-        // when + then
-        Assertions.assertThrows(NotProperAuthorityException.class, () -> userService.deleteUser(1L, 2L));
-    }
-
-    @Test
-    @DisplayName("deleteUser - 정상동작")
-    void testDeleteUser_3(){
-        // given
-        when(
-                userRepository.findById(1L)
-        ).thenReturn(
-                Optional.of(User.builder().id(1L).build())
-        );
-        // when
-        userService.deleteUser(1L, 1L);
-        // then
-        verify(userRepository, times(1)).delete(any());
-    }
 }
