65e31f0049132a153b4bdec4e27430e63e2348b4
Taeyoung-Yang <110372475+Taeyoung-Yang>
2023-09-17 00:22:00 +0900

    [#118] 테스트 환경 임시 구성

--- src/test/java/com/plogcareers/backend/BackendApplicationTests.java
+++ src/test/java/com/plogcareers/backend/BackendApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
-@ActiveProfiles("local")
+@ActiveProfiles("test")
 class BackendApplicationTests {
 
 	@Test
