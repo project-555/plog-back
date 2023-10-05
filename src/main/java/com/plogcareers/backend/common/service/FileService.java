1fa6edef1169f3b1758d53304e3647c0ad23a233
YangTaeyoung <59782504+YangTaeyoung@users.noreply.github.com>
2023-10-06 02:32:05 +0900

    [#117] 오라클 관련 파일 삭제

--- src/main/java/com/plogcareers/backend/common/service/FileService.java
+++ src/main/java/com/plogcareers/backend/common/service/FileService.java
@@ -1,33 +1,15 @@
 package com.plogcareers.backend.common.service;
 
-import com.plogcareers.backend.common.component.PlogObjectStorageClient;
 import com.plogcareers.backend.common.domain.dto.GeneratePreSignedURLRequest;
 import com.plogcareers.backend.common.domain.dto.GeneratePreSignedURLResponse;
-import com.plogcareers.backend.common.domain.dto.UploadFileRequest;
-import com.plogcareers.backend.common.domain.dto.UploadFileResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.net.URL;
-import java.util.Base64;
 
 @Service
 @RequiredArgsConstructor
 public class FileService {
 
-    private final PlogObjectStorageClient objectStorageClient;
     private final S3Service s3Service;
-
-    public UploadFileResponse uploadFile(UploadFileRequest request) throws IOException {
-        // 이미지 파일 Base64 Encoded String을 Decode하여 byte array로 변환
-        byte[] decodedBytes = Base64.getDecoder().decode(request.getFileBase64());
-
-        // 파일 업로드
-        String uploadedFileURL = objectStorageClient.uploadFile(new ByteArrayInputStream(decodedBytes));
-        return UploadFileResponse.builder().uploadedFileURL(uploadedFileURL).build();
-    }
 
     public GeneratePreSignedURLResponse generatePreSignedURL(GeneratePreSignedURLRequest request) {
         // Pre-Signed URL 생성
