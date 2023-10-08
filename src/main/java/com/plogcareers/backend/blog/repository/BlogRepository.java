78504831bffae4d5c298271adaf48b10ecc66461
aiaaua <yse00800@gmail.com>
2023-10-08 22:41:46 +0900

    [#133] repository test 시 test DB를 띄워 테스트하도록 세팅 및 예시 작성

--- src/main/java/com/plogcareers/backend/blog/repository/BlogRepository.java
+++ src/main/java/com/plogcareers/backend/blog/repository/BlogRepository.java
@@ -3,9 +3,11 @@ package com.plogcareers.backend.blog.repository;
 import com.plogcareers.backend.blog.domain.entity.Blog;
 import com.plogcareers.backend.ums.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface BlogRepository extends JpaRepository<Blog, Long> {
     Boolean existsByBlogName(String blogName);
 
