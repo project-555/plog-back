61da8982db331c6400740ee10cd82fd42550fa1c
YangTaeyoung <59782504+YangTaeyoung@users.noreply.github.com>
2023-10-15 01:56:35 +0900

    [#149] FileService 테스트 코드 작성

--- src/main/java/com/plogcareers/backend/common/service/FileService.java
+++ src/main/java/com/plogcareers/backend/common/service/FileService.java
@@ -1,5 +1,6 @@
 package com.plogcareers.backend.common.service;
 
+import com.plogcareers.backend.common.component.AWSS3Client;
 import com.plogcareers.backend.common.domain.dto.GeneratePreSignedURLRequest;
 import com.plogcareers.backend.common.domain.dto.GeneratePreSignedURLResponse;
 import lombok.RequiredArgsConstructor;
@@ -9,11 +10,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class FileService {
 
-    private final S3Service s3Service;
+    private final AWSS3Client awsS3Client;
 
     public GeneratePreSignedURLResponse generatePreSignedURL(GeneratePreSignedURLRequest request) {
         // Pre-Signed URL 생성
-        String preSignedURL = s3Service.generatePUTPresignedURL(request.getFileName(), request.getContentType());
+        String preSignedURL = awsS3Client.generatePUTPresignedURL(request.getFileName(), request.getContentType());
 
         return GeneratePreSignedURLResponse.builder()
                 .preSignedURL(preSignedURL)
